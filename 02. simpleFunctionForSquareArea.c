035b4b20e54077b7a12c2fc1942d43c8dbd8bfa7
kadoubleU <108259380+kadoubleU@users.noreply.github.com>
2022-07-07 10:44:14 +0300

    Create 02. simpleFunctionForSquareArea.c

---
+++ 02. simpleFunctionForSquareArea.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+float squareArea(float a);
+
+int main() {
+	
+	float side, result;
+	
+	printf("Enter the length of the square: ");
+	scanf("%f", &side);
+	
+	result = squareArea(side);
+	
+	printf("The Square Area is: %.2f", result);
+	
+}
+
+float squareArea(float a) {
+	float area;
+	area = a * a;
+	
+	return area;
+}
