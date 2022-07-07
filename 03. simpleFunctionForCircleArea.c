8dc5b41b9591fd3221f2b131efcbccd5adb0a80f
kadoubleU <108259380+kadoubleU@users.noreply.github.com>
2022-07-07 11:13:26 +0300

    Create 03. simpleFunctionForCircleArea.c

---
+++ 03. simpleFunctionForCircleArea.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <math.h>
+
+float circleArea(float);
+
+int main() {
+	float radius, result;
+	
+	printf("Enter the circle radius: ");
+	scanf("%f", &radius);
+	
+	result = circleArea(radius);
+	printf("The Circle Area Radius is: %.2f", result);
+}
+
+float circleArea(float r) {
+	float area;
+	
+	area = M_PI * r * r;
+}
